24b9f2ba3ad7e82a4d341ca6142620724401e94b
Castorri Lorenzo <loricastorri@gmail.com>
2025-12-09 12:28:41 +0100

    122: Create implementation for the interface 'Logics.java'

---
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -0,0 +1,31 @@
+package it.unibo.es2;
+
+import java.util.List;
+
+public class LogicsImpl implements Logics {
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public List<List<Boolean>> grid() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'grid'");
+    }
+
+    @Override
+    public <X, Y> boolean hit(Pair<X, Y> pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hit'");
+    }
+
+    @Override
+    public boolean toQuit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+    }
+    
+}
